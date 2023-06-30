90a9cc07a3954ccb13066adbdfbea2b81ba2352c
Patricio <patricio.lyono@gmail.com>
2023-06-30 00:07:17 -0400

    react memory

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Cards from './components/Cards'
+import { useState } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Memory Game - REACTjs</h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
